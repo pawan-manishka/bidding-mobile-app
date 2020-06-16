231ea01023995b6de8f241e954a9a38b0fdd78c0
Shanu95 <shannirmala95@gmail.com>
2020-06-16 16:47:54 +0530

    ui changes in Quotes.js. added react-native-signature-capture and react-native-tesseract-ocr to the project. canvas clear is only working for the last row price.

--- biddingMobile/src/screens/Quotes.js
+++ biddingMobile/src/screens/Quotes.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState,useRef} from 'react';
 //import {Context as AuthContext} from "../context/AuthContext";
 import {
     StyleSheet,
@@ -13,14 +13,15 @@ import {
 } from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Entypo from 'react-native-vector-icons/Entypo';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Main from '../navigation/RootStack';
+import SignatureCapture from 'react-native-signature-capture';
 
 const Quotes = () => {
 
     //const {state, signIn} = useContext(AuthContext);
-    const [UserName, setUserName] = useState('testextra2');
-    const [Password, setPassword] = useState('test123');
-    const [Progressbar, setProgressbar] = useState(false);
-    const [Errormessage, setErrorMessage] = useState('');
+    //let signComponent= null;
+    let signComponent = React.createRef();
 
 
     function renderSeparator() {
@@ -35,9 +36,14 @@ const Quotes = () => {
         );
     };
 
+    function resetSign() {
+        //console.log("function call: "+signComponent.current.resetImage())
+        signComponent.current.resetImage();
+    }
+
 
     return (
-        <View style={{width:'100%'}}>
+        <View style={{width: '100%'}}>
             <FlatList
                 data={[
                     {key: 'Android'}, {key: 'iOS'}, {key: 'Java'}, {key: 'Swift'},
@@ -46,58 +52,184 @@ const Quotes = () => {
                     {key: 'Ruby'}, {key: 'Rails'}, {key: '.Net'},
                     {key: 'Perl'},
                 ]}
-                renderItem={({item}) =>(
-                    <View style={{flex: 1, flexDirection: 'row', backgroundColor: '#0b1224',justifyContent: 'center',
-                        alignItems: 'stretch',padding:5}}>
-                        <View style={{flexDirection: 'column',flex:0.5,justifyContent: 'center',
-                            alignItems: 'center'}}>
+                renderItem={({item}) => (
+                    <View style={{
+                        flex: 1, flexDirection: 'row', backgroundColor: '#0b1224', justifyContent: 'center',
+                        alignItems: 'stretch', padding: 5,
+                    }}>
+                        <View style={{
+                            flexDirection: 'column', flex: 0.5, justifyContent: 'center',
+                            alignItems: 'center',
+                        }}>
                             {/*<Text style={{color: 'white'}}>{item.key}</Text>*/}
-                            <Text style={{color: 'white',fontWeight:'bold',fontSize:16}}>AUCO45</Text>
-                            <Text style={{color: 'white',fontSize:12}}>Spread 3</Text>
-                            <Text style={{color: 'white',fontSize:12}}>18:53:30</Text>
+                            <Text style={{color: 'white', fontWeight: 'bold', fontSize: 16}}>AUCO45</Text>
+                            <Text style={{color: 'white', fontSize: 12}}>Spread 3</Text>
+                            <Text style={{color: 'white', fontSize: 12}}>18:53:30</Text>
                         </View>
-                        <View style={{flexDirection: 'row',flex:1.3,justifyContent: 'center',
-                            alignItems: 'center'}}>
-                            <View style={{flexDirection: 'column',justifyContent:'center'}}>
+                        <View style={{
+                            flexDirection: 'row', flex: 1.3, justifyContent: 'center',
+                            alignItems: 'center',
+                        }}>
+                            <View style={{flexDirection: 'column', justifyContent: 'center'}}>
                                 <View style={{flexDirection: 'row', alignItems: 'flex-start'}}>
-                                    <Text style={{fontSize: 20, lineHeight: 30,color:'red'}}>1.08</Text>
-                                    <Text style={{fontSize: 25, lineHeight: 30,color:'red'}}>04</Text>
-                                    <Text style={{fontSize: 11, lineHeight: 18,color:'red'}}>9</Text>
+                                    <Text style={{fontSize: 20, lineHeight: 30, color: 'red'}}>1.08</Text>
+                                    <Text style={{fontSize: 25, lineHeight: 30, color: 'red'}}>04</Text>
+                                    <Text style={{fontSize: 11, lineHeight: 18, color: 'red'}}>9</Text>
                                 </View>
-                                <View style={{flexDirection:'row'}}>
+                                <View style={{flexDirection: 'row'}}>
                                     <Entypo
                                         name="arrow-long-down" size={15}
                                         color="red"/>
-                                    <Text style={{color: 'red',fontSize:12}}>1.08026</Text>
+                                    <Text style={{color: 'red', fontSize: 12}}>1.08026</Text>
                                 </View>
 
                             </View>
-                            <View style={{flexDirection: 'column',paddingLeft:5,paddingRight:5,justifyContent:'center'}}>
+                            <View style={{
+                                flexDirection: 'column',
+                                paddingLeft: 5,
+                                paddingRight: 5,
+                                justifyContent: 'center',
+                            }}>
                                 <View style={{flexDirection: 'row', alignItems: 'flex-start'}}>
-                                    <Text style={{fontSize: 20, lineHeight: 30,color:'red'}}>1.08</Text>
-                                    <Text style={{fontSize: 25, lineHeight: 30,color:'red'}}>05</Text>
-                                    <Text style={{fontSize: 11, lineHeight: 18,color:'red'}}>2</Text>
+                                    <Text style={{fontSize: 20, lineHeight: 30, color: 'red'}}>1.08</Text>
+                                    <Text style={{fontSize: 25, lineHeight: 30, color: 'red'}}>05</Text>
+                                    <Text style={{fontSize: 11, lineHeight: 18, color: 'red'}}>2</Text>
                                 </View>
-                                <View style={{flexDirection:'row'}}>
+                                <View style={{flexDirection: 'row'}}>
                                     <Entypo
                                         name="arrow-long-up" size={15}
                                         color="green"/>
-                                    <Text style={{color: 'green',fontSize:12}}>1.08026</Text>
+                                    <Text style={{color: 'green', fontSize: 12}}>1.08026</Text>
                                 </View>
                             </View>
                         </View>
-                        <View style={{flexDirection:'row',flex:2,justifyContent: 'center',
-                            alignItems: 'center'}}>
-                            <View style={{width:'40%',height:80,backgroundColor:'#192535',borderRadius:10,margin:2}}>
-                                <Text style={{color: 'white',paddingLeft:8,paddingTop:8}}>Price</Text>
+                        <View style={{
+                            flexDirection: 'row', flex: 2, justifyContent: 'center',
+                            alignItems: 'center',
+                        }}>
+                            <View style={{
+                                width: '40%',
+                                height: 80,
+                                backgroundColor: '#192535',
+                                borderRadius: 10,
+                                margin: 2,
+                            }}>
+                                <Text style={{color: 'white', paddingLeft: 8, paddingTop: 2}}>Price</Text>
+                                <SignatureCapture
+                                    style={{height: 35,marginLeft: 5,marginRight: 5,}}
+                                    ref={signComponent}
+                                    // onSaveEvent={this._onSaveEvent}
+                                    // onDragEvent={this._onDragEvent}
+                                    saveImageFileInExtStorage={true}
+                                    showNativeButtons={false}
+                                    showTitleLabel={false}
+                                    viewMode={"portrait"}
+                                    backgroundColor={'#192535'}
+                                    strokeColor="white"
+                                    maxStrokeWidth={0.1}
+                                />
+                                <View style={{
+                                    flexDirection: 'row',
+                                    justifyContent:'flex-end',
+                                    marginTop:2
+                                }}>
+                                    <TouchableOpacity
+                                        style={[
+                                            styles.button,
+                                            {
+                                                borderColor: 'green',
+                                                borderWidth: 1,
+                                                borderRadius: 50,
+                                                width: 20,
+                                                height: 20,
+                                                marginLeft: 5,
+                                                marginRight: 5
+                                            },
+                                        ]}>
+                                        <MaterialIcons name='done' size={18}
+                                                       color="green"/>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity
+                                        onPress={() => { resetSign() } }
+                                        style={[
+                                            styles.button,
+                                            {
+                                                borderColor: 'red',
+                                                borderWidth: 1,
+                                                borderRadius: 50,
+                                                width: 20,
+                                                height: 20,
+                                                marginLeft: 5,
+                                                marginRight: 5
+                                            },
+                                        ]}>
+                                        <MaterialIcons name='clear' size={18}
+                                                       color="red"/>
+                                    </TouchableOpacity>
+                                </View>
                             </View>
-                            <View style={{width:'55%',height:80,backgroundColor:'#192535',borderRadius:10,margin:2}}>
-                                <Text style={{color: 'white',paddingLeft:8,paddingTop:8}}>Remarks</Text>
+                            <View style={{
+                                width: '55%',
+                                height: 80,
+                                backgroundColor: '#192535',
+                                borderRadius: 10,
+                                margin: 2,
+                            }}>
+                                <Text style={{color: 'white', paddingLeft: 8, paddingTop: 2}}>Remarks</Text>
+                                <SignatureCapture
+                                    style={{height: 35,marginLeft: 5,marginRight: 5,}}
+                                    //ref={signComponent}
+                                    // onSaveEvent={this._onSaveEvent}
+                                    // onDragEvent={this._onDragEvent}
+                                    saveImageFileInExtStorage={true}
+                                    showNativeButtons={false}
+                                    showTitleLabel={false}
+                                    viewMode={"portrait"}
+                                    backgroundColor={'#192535'}
+                                    strokeColor="white"
+                                    maxStrokeWidth={0.1}
+                                />
+                                <View style={{
+                                    flexDirection: 'row',justifyContent:'flex-end',marginTop:2
+                                }}>
+                                    <TouchableOpacity
+                                        style={[
+                                            styles.button,
+                                            {
+                                                borderColor: 'green',
+                                                borderWidth: 1,
+                                                borderRadius: 50,
+                                                width: 20,
+                                                height: 20,
+                                                marginLeft: 5,
+                                                marginRight: 5
+                                            },
+                                        ]}>
+                                        <MaterialIcons name='done' size={18}
+                                                       color="green"/>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity
+                                        style={[
+                                            styles.button,
+                                            {
+                                                borderColor: 'red',
+                                                borderWidth: 1,
+                                                borderRadius: 50,
+                                                width: 20,
+                                                height: 20,
+                                                marginLeft: 5,
+                                                marginRight: 5
+                                            },
+                                        ]}>
+                                        <MaterialIcons name='clear' size={18}
+                                                       color="red"/>
+                                    </TouchableOpacity>
+                                </View>
                             </View>
                         </View>
 
                     </View>
-                    )}
+                )}
                 ItemSeparatorComponent={renderSeparator}
             />
         </View>
@@ -121,7 +253,7 @@ const styles = StyleSheet.create({
     ,
     container: {
         flex: 1,
-        width:'100%',
+        width: '100%',
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: 'white',
